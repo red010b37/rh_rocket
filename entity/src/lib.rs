e1be0bf55cd134bc9f567209b0dae926905ee672
paul <paul@sandersonhowe.nz>
2022-11-29 18:42:03 +1300

    making this thing

--- entity/src/lib.rs
+++
@@ -1,5 +0,0 @@
-#[macro_use]
-extern crate rocket;
-
-pub mod post;
-pub mod job;
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    rh_rocket::main();
+#[macro_use] extern crate rocket;
+
+#[get("/")]
+async fn index() -> &'static str {
+    "Hello, world!"
+}
+
+#[rocket::main]
+async fn main() {
+ rocket::build().mount("/", routes![
+ index]).launch().await;
 }
