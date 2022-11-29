2ab2cfc96d3378aefe6ba8a3f1326733bf25d5cf
paul <paul@sandersonhowe.nz>
2022-11-29 19:48:01 +1300

    making changes

--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,5 @@
-use log::LevelFilter;
+
 
 pub mod errors;
 pub mod routes;
 
-fn setup_logger() {
-    let (level, logger) = fern::Dispatch::new()
-        .format(move | out, message, record| {
-            out.finish(format_args!(
-                "[{date}] [{level}][{target}] [{message}]",
-                date = chrono::Local::now().format("[%Y-%m-%d][%H:%M:%S%.3f]"),
-                target = record.target(),
-                level = record.level(),
-                message = message
-            ))
-        })
-        .level(LevelFilter::Info)
-        .chain(std::io::stdout())
-        .chain(
-            fern::log_file("logs/application.log")
-                .unwrap_or_else(|_| panic!("Cannot open logs/application.log")),
-        )
-        .into_log();
-    async_log::Logger::wrap(logger, || 0).start(level).unwrap();
-}
